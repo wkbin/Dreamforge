edadbee40cb93e5d9ee46f667bc711d1031274a4
wkbin <wangkebin1997@gmail.com>
2026-05-10 03:44:57 +0800

    Restore mobile scrolling for dialogue layout after composer pin

--- src/web/static/js/bootstrap.js
+++ src/web/static/js/bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = "20260509193232";
+  const version = "20260509194241";
   window.__ZAOMENG_WEB_UI_VERSION__ = version;
   const rootFragments = [
     { id: "header-root", url: `/web/fragments/header.html?v=${version}` },
